f40736d866fb093d63f775f188c129a5f50ba3b0
Shakeel Soogun <shakeel@serendipityai.co.uk>
2021-04-20 10:42:16 +0100

    Render components during normalization  - Allows defining separate functions of components as JSX

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,7 @@
 import { InternalPresentation } from "./normalizer";
 import React from "react";
 import ReactIs from "react-is";
+import { isReactPPTXComponent } from "./nodes";
 
 export const POINTS_TO_INCHES = 1 / 72;
 
@@ -34,26 +35,38 @@ type PotentialChildren = Array<
 >;
 export function flattenChildren(
   children: React.ReactNode,
-  depth = 0,
   keys: (string | number)[] = []
 ): PotentialChildren {
   return React.Children.toArray(children).reduce(
-    (acc: PotentialChildren, node: React.ReactNode, nodeIndex) => {
+    (acc: PotentialChildren, node, nodeIndex) => {
       if (ReactIs.isFragment(node)) {
         acc.push(
           ...flattenChildren(
             node.props.children,
-            depth + 1,
             keys.concat(node.key || nodeIndex)
           )
         );
       } else {
-        if (React.isValidElement(node)) {
-          acc.push(
-            React.cloneElement(node, {
-              key: keys.concat(String(node.key)).join("."),
-            })
-          );
+        if (ReactIs.isElement(node)) {
+          if (isReactPPTXComponent(node)) {
+            acc.push(
+              React.cloneElement(node, {
+                key: keys.concat(String(node.key)).join("."),
+              })
+            );
+          } else {
+            // A component that could return/contain react-pptx components,
+            // traverse the tree some more
+            let children = node.props.children;
+            if (node.type instanceof Function) {
+              children = (node.type as React.FunctionComponent<any>)(
+                node.props
+              );
+            }
+            acc.push(
+              ...flattenChildren(children, keys.concat(node.key || nodeIndex))
+            );
+          }
         } else if (typeof node === "string" || typeof node === "number") {
           acc.push(node);
         }
